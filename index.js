31943dc9cf80ff09dc24c57d0b5b7580e79ff20e
Winston  Chung <18.chung.winston@ctkprep.org>
2020-05-09 00:03:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your Circle class here
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+  
+  get diameter() {
+    return this.radius * 2
+  }
+  
+  get circumference() {
+    return Math.PI*(this.radius * 2)
+  }
+  
+  get area() {
+    return Math.PI * (this.radius**2)
+  }
+  
+  set diameter(newDiameter) {
+      return this.radius = newDiameter/2
+  }
+  
+  set circumference(newCircumference) {
+    return this.radius = newCircumference/(Math.PI*2)  
+  }
+  
+  set area(newArea) {
+    let newNewArea = newArea/Math.PI
+    return this.radius = Math.sqrt(newNewArea)
+  }
+}
